b87d70cc0a9634a2472f9a5691832f4b7a7b1acb
wesleykg <wesley.gerelle@gmail.com>
2016-01-20 15:10:08 -0800

    Initialize

---
+++ 01_getting-started.R
@@ -0,0 +1,8 @@
+## Getting Started exercises
+library(downloader)
+url <- "https://raw.githubusercontent.com/genomicsclass/dagdata/master/inst/extdata/femaleMiceWeights.csv"
+filename <- "femaleMiceWeights.csv" 
+download(url, destfile=filename)
+
+#1
+micedat <- read.csv(filename)
