73223b731dd2e834e25e1b8f7f2599728cd59244
wesleykg <wesley.gerelle@gmail.com>
2016-01-22 12:12:09 -0800

    starting random variable exercises

---
+++ week2/random-variables-prob-distrib.R
@@ -0,0 +1,9 @@
+## Random variable exercises
+
+library(downloader) 
+url <- "https://raw.githubusercontent.com/genomicsclass/dagdata/master/inst/extdata/femaleControlsPopulation.csv"
+filename <- basename(url)
+download(url, destfile=filename)
+x <- unlist( read.csv(filename) ) # weight for entire population
+
+#1
