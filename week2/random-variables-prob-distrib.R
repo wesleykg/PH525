7a1857cbfee30fac629f37c598b7a153a5ec3d8b
wesleykg <wesley.gerelle@gmail.com>
2016-01-22 15:29:25 -0800

    beginning null distribution exercises

--- week2/random-variables-prob-distrib.R
+++ week2/random-variables-prob-distrib.R
@@ -18,3 +18,21 @@ abs(mean(x) - mean(ransam))
 set.seed(5)
 ransam <- sample(x, size = 5)
 abs(mean(x) - mean(ransam))
+
+## Null distribution exercises
+
+library(downloader) 
+url <- "https://raw.githubusercontent.com/genomicsclass/dagdata/master/inst/extdata/femaleControlsPopulation.csv"
+filename <- basename(url)
+download(url, destfile=filename)
+x <- unlist( read.csv(filename) ) # weight for entire population
+
+#1
+
+
+
+
+
+
+
+
