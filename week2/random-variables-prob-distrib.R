ba1aefb63521c017ed70af9389008657a65f56d4
wesleykg <wesley.gerelle@gmail.com>
2016-01-25 12:13:53 -0800

    changed varname x to population

--- week2/random-variables-prob-distrib.R
+++ week2/random-variables-prob-distrib.R
@@ -1,34 +1,34 @@
-## Random variable exercises
+## Random variable epopulationercises
 
 library(downloader) 
-url <- "https://raw.githubusercontent.com/genomicsclass/dagdata/master/inst/extdata/femaleControlsPopulation.csv"
+url <- "https://raw.githubusercontent.com/genomicsclass/dagdata/master/inst/epopulationtdata/femaleControlsPopulation.csv"
 filename <- basename(url)
 download(url, destfile=filename)
-x <- unlist( read.csv(filename) ) # weight for entire population
+population <- unlist( read.csv(filename) )
 
 #1
-mean(x)
+mean(population)
 
 #2
 set.seed(1)
-ransam <- sample(x, size = 5)
-abs(mean(x) - mean(ransam))
+ransam <- sample(population, size = 5)
+abs(mean(population) - mean(ransam))
 
 #3
 set.seed(5)
-ransam <- sample(x, size = 5)
-abs(mean(x) - mean(ransam))
+ransam <- sample(population, size = 5)
+abs(mean(population) - mean(ransam))
 
-## Null distribution exercises
-
-library(downloader) 
-url <- "https://raw.githubusercontent.com/genomicsclass/dagdata/master/inst/extdata/femaleControlsPopulation.csv"
-filename <- basename(url)
-download(url, destfile=filename)
-x <- unlist( read.csv(filename) ) # weight for entire population
+## Null distribution epopulationercises
 
 #1
 
-
+n <- 1000
+null <- vector("numeric",n)
+for (i in 1:n) {
+  control <- sample(population,12)
+  treatment <- sample(population,12)
+  null[i] <- mean(treatment) - mean(control)
+}
 
 
