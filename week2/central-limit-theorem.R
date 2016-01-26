f70fb0934cc42de99de664029a9cbae8bd7f178e
wesleykg <wesley.gerelle@gmail.com>
2016-01-26 08:15:23 -0800

    started norm distrib exercises

---
+++ week2/central-limit-theorem.R
@@ -0,0 +1,25 @@
+## Normal distribution exercises
+
+library(downloader) 
+url <- "https://raw.githubusercontent.com/genomicsclass/dagdata/master/inst/extdata/femaleControlsPopulation.csv"
+filename <- basename(url)
+download(url, destfile=filename)
+x <- unlist( read.csv(filename) ) #population weights
+
+set.seed(1)
+n <- 1000
+averages5 <- vector("numeric",n)
+for(i in 1:n){
+  X <- sample(x, 5)
+  averages5[i] <- mean(X)
+}
+
+set.seed(1)
+n <- 1000
+averages50 <- vector("numeric",n)
+for(i in 1:n){
+  X <- sample(x, 50)
+  averages5[i] <- mean(X)
+}
+
+#1
